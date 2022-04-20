55bd475d0d25a70a7663a28bd68b664d8e15aa88
Lennix o Robles <94206190+L3nn1x@users.noreply.github.com>
2022-04-20 10:22:54 -0400

    Update main.py

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 import json
 import numpy
 
+
 class TheMatrixHash:
     def __init__(self):
 
@@ -49,15 +50,18 @@ class TheMatrixHash:
     def createMatrixHash(self, passwd=str):
         bta = bytearray(passwd, 'utf-8')
         mv = list(memoryview(bta))
-       
+
+
+        # print(mv) # layer 1
 
         matrix = []
         hashed = []
         for n in range(len(passwd)):
             stde = int(numpy.median(mv[n])) + int(numpy.mean(mv[n])) / int(numpy.sum(mv))
 
-            matrix.append([i+stde for i in range(1, len(passwd) + 1)])
+            matrix.append([i + stde for i in range(1, len(passwd) + 1)])
 
+        #print(matrix) #layer 2
 
         for i in range(len(mv)):
             num = mv[i]
@@ -65,10 +69,9 @@ class TheMatrixHash:
                 num -= 3
                 continue
             if num < len(passwd):
-                shiftH = self.hShift(matrix, num+1, i + 1)
+                shiftH = self.hShift(matrix, num + 1, i + 1)
                 matrix[:] = shiftH
                 continue
-
 
         for i in range(len(mv)):
             num = mv[i]
@@ -76,24 +79,34 @@ class TheMatrixHash:
                 num -= 4
                 continue
             if num < len(passwd):
-                shiftV = self.vShift(matrix, -num+1, i + 1)
+                shiftV = self.vShift(matrix, -num + 1, i + 1)
                 matrix[:] = shiftV
                 continue
 
-
         for i in range(len(matrix)):
-            numberlist = [n+mv[i-1]+len(str(mv[i-1])) for n in matrix[i]]
+            numberlist = [n + mv[i - 1] + len(str(mv[i - 1])) for n in matrix[i]]
             hashed.append(numberlist)
 
-        #print(numpy.vstack(hashed))
-        hash_matrix = []
+        # print(numpy.vstack(hashed)) #layer 3
+
+        matrix.clear()
         for numberlist in hashed:
-            numberlist[:] = [' '.join(format(ord(b), 'b') .replace('0', self.sample[0]).replace('1', self.sample[1]) for b in str(i)) for i in numberlist]
-            hash_matrix.append(numberlist)
+            matrix.append(numpy.sum(numberlist))
 
+        #print(numpy.vstack(matrix)) # 4
 
+        allSum = str(numpy.sum(matrix))
 
-        return hash_matrix
+        #print(allSum) #layer 5
+
+        matrix_hash = []
+        for hashedBin in ' '.join(format(ord(b), 'b').replace('0', self.sample[1]).replace('1', self.sample[0]) for b in allSum).split():
+            matrix_hash.append([hashedBin])
+
+        #print(matrix_hash) #layer 6
+
+        return matrix_hash
+
 
 ### You can delete section 1#
 
@@ -104,12 +117,12 @@ while True:
         tmh = TheMatrixHash()
         user = input('password: ')
         matrix_hash = tmh.createMatrixHash(passwd=user)
-        
 
+        #with open('matrixHash.json', 'w') as f:
+         #   p = json.dump(matrix_hash, f)
 
         with open('matrixHash.json', 'r') as f:
             p = json.load(f)
-
 
             if p == matrix_hash:
                 print('ACCESS: ', True)
